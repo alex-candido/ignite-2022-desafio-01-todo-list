c2360d5e9c1bb3cf18d7d093f71eff363cd26dfa
Alex Cândido <alex.candido.tec@gmail.com>
2022-09-07 16:26:12 -0300

    funções de submit para add taskTitle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
 import todoLogo from '../../assets/todoLogo.svg';
 import styles from './header.module.css';
@@ -8,13 +8,29 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ onAddTask }) => {
+  const [title, setTitle] = React.useState('');
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    onAddTask(title);
+    setTitle('');
+  }
+
+  function onChangTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
   return (
     <header className={styles.header}>
       <img src={todoLogo} alt="ToDo Logo" />
 
-      <form className={styles.newTaskForm}>
-        <input placeholder="Adicione uma nova tarefa" />
-        <button type="button">
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          onChange={onChangTitle}
+          value={title}
+        />
+        <button type="submit">
           Criar
           <AiOutlinePlusCircle size={20} />
         </button>
